1b1e472c96dfe9db5301ddaebbbd908bd3b46845
Adam Kiczula <ackiczula@gmail.com>
2022-08-01 22:48:55 -0500

    Init gem

---
+++ spec/dinglehopper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dinglehopper do
+  it "has a version number" do
+    expect(Dinglehopper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
